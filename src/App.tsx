820bcf8e225fb4227c5edeb4a8431b488add3e9a
itmente1 <itmente2022@gmail.com>
2022-03-15 09:46:54 +0600

    basic project structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import TheLayout from "./views/pages/TheLayout";
+
+function App() {
+  return (
+    <div className="App">
+      <TheLayout/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/pages/TheLayout.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Footer from './common/Footer';
+import Header from './common/Header';
+import Main from './common/Main';
+
+const TheLayout = () => {
+  return (
+    <div>
+      <div>
+        <Header />
+      </div>
+      <main>
+        <Main />
+      </main>
+      <div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default TheLayout;
